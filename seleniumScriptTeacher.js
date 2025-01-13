dabe35144d16b0ff16d97d1427002a2a8e770562
aaryanCompro <aaryan.saraswat@comprotechnologies.com>
2025-01-13 15:39:15 +0530

    Update 13

--- seleniumScriptTeacher.js
+++ seleniumScriptTeacher.js
@@ -8,31 +8,4 @@ const config = {
       bundleCardToggleButtonIdReplacer: "rl-25",
     },
   },
-  "RL-25 B1": {
-    skills: {
-      Reading: false,
-    },
-    selectors: {
-      bundleCardToggleButtonIdReplacer: "rl-25",
-    },
-  },
-  "Personalised Learning A2": {
-    skills: {
-      Reading: false,
-      Listening: false,
-    },
-    selectors: {
-      bundleCardToggleButtonIdReplacer: "mosaic",
-    },
-  },
-  "Personalised Learning B2": {
-    skills: {
-      Reading: false,
-      Listening: false,
-    },
-    selectors: {
-      bundleCardToggleButtonIdReplacer: "mosaic",
-    },
-  },
 };
-
