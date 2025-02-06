19dfccd57239ea665dc834cf0e5b8f1efa2f5cdc
aaryanCompro <aaryan.saraswat@comprotechnologies.com>
2025-02-06 12:36:50 +0530

    Updated Teacher

--- seleniumScriptTeacher.js
+++ seleniumScriptTeacher.js
@@ -1,14 +1,2 @@
 console.log("Test To check error codes");
 console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
-console.log("Test To check error codes");
